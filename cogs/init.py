2dd17f40a5d8e51a05e7217b909006c185c3684c
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-03 00:52:13 +0900

    Update init.py

--- cogs/init.py
+++ cogs/init.py
@@ -42,7 +42,10 @@ class InitCog(commands.Cog):
         )
 
 async def setup(bot):
-    await bot.add_cog(InitCog(bot))
-    for cmd in bot.tree.get_commands():
+    cog = THIS_COG_CLASS(bot)
+    await bot.add_cog(cog)
+
+    for cmd in cog.get_app_commands():
         for gid in bot.GUILD_IDS:
             bot.tree.add_command(cmd, guild=discord.Object(id=gid))
+
