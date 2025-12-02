846fa8b1bffec45ebeac6ffbcf2370bac31f29e5
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-02 23:52:48 +0900

    Update balance.py

--- cogs/balance.py
+++ cogs/balance.py
@@ -84,4 +84,6 @@ async def setup(bot):
     await bot.add_cog(cog)
 
     for cmd in cog.get_app_commands():
-        bot.tree.add_command(cmd, guild=discord.Object(id=1420918259187712093))
+        for gid in bot.GUILD_IDS:
+            bot.tree.add_command(cmd, guild=discord.Object(id=gid))
+
