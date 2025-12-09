85555f6af08acc2ee18004f2af8029172510f0c4
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-09 16:58:55 +0900

    Update gamble.py

--- cogs/gamble.py
+++ cogs/gamble.py
@@ -459,9 +459,10 @@ class JudgeView(discord.ui.View):
 # setup（必須）
 # --------------------------
 async def setup(bot):
-    cog = AdminCog(bot)
+    cog = GambleCog(bot)
     await bot.add_cog(cog)
 
     for cmd in cog.get_app_commands():
         for gid in bot.GUILD_IDS:
             bot.tree.add_command(cmd, guild=discord.Object(id=gid))
+
