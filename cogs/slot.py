356d552205aedeaf37fa7b7304dec86fde689a42
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-16 19:01:05 +0900

    Update slot.py

--- cogs/slot.py
+++ cogs/slot.py
@@ -304,4 +304,12 @@ class SlotContinueView(discord.ui.View):
 
 
 async def setup(bot):
-    await bot.add_cog(SlotCog(bot))
+    cog = SlotCog(bot)
+    await bot.add_cog(cog)
+
+    for cmd in cog.get_app_commands():
+        for gid in bot.GUILD_IDS:
+            bot.tree.add_command(
+                cmd,
+                guild=discord.Object(id=gid)
+            )
