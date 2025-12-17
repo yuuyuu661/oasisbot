506d4e6353d1abe076ed5ab037dcc49771943f5b
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-17 15:36:30 +0900

    Update setup.py

--- cogs/hotel/setup.py
+++ cogs/hotel/setup.py
@@ -1,11 +1,9 @@
 import discord
+from discord.ext import commands
+
 from .hotel_cog import HotelCog
 
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(HotelSetupCog(bot))
-
+    await bot.add_cog(HotelCog(bot))
     print("🏨 Hotel module loaded.")
-
-
-
