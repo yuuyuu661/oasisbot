c213233ceacfad24ebff2847ac8b1c162c463887
yuuyuu661 <liuliliuli71@gmail.com>
2025-12-17 13:00:45 +0900

    Update setup.py

--- cogs/hotel/setup.py
+++ cogs/hotel/setup.py
@@ -12,6 +12,12 @@ async def setup(bot):
     # 指定ギルドにスラッシュコマンドを同期
     if hasattr(bot, "GUILD_IDS"):
         for cmd in cog.get_app_commands():
+                            # 🔒 すでに登録済みならスキップ
+        if cmd.name in bot._added_app_commands:
+            continue
+
+        # ✅ 初回登録
+        bot._added_app_commands.add(cmd.name)
             for gid in bot.GUILD_IDS:
                 bot.tree.add_command(cmd, guild=discord.Object(id=gid))
 
