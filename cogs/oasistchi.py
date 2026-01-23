aa41a19a1bff4506654abd0e2d13a0346471366d
yuuyuu661 <liuliliuli71@gmail.com>
2026-01-23 14:02:59 +0900

    Update oasistchi.py

--- cogs/oasistchi.py
+++ cogs/oasistchi.py
@@ -1007,31 +1007,3 @@ async def setup(bot):
     for cmd in cog.get_app_commands():
         for gid in bot.GUILD_IDS:
             bot.tree.add_command(cmd, guild=discord.Object(id=gid))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
