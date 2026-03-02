5a3fc605f42dab3c0d5e71b55f40f439bb8f84e6
yuuyuu661 <liuliliuli71@gmail.com>
2026-03-02 17:22:19 +0900

    Update web_api.py

--- web_api.py
+++ web_api.py
@@ -127,10 +127,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://racetest-production.up.railway.app",
-        "https://oasisbot-production.up.railway.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
