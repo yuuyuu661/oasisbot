3d8450c7b360c713ce9a69c51a09081af58c4f33
yuuyuu661 <liuliliuli71@gmail.com>
2026-03-14 02:13:09 +0900

    Update balance.py

--- cogs/balance.py
+++ cogs/balance.py
@@ -205,7 +205,7 @@ class BalanceCog(commands.Cog):
             if not intro_link:
                 return
 
-            send_channel = after.channel.text_channel
+            send_channel = after.channel
 
             if not send_channel:
                 return
