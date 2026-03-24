dcbb8a10facb19ac0353331946fe0955de30897c
yuuyuu661 <liuliliuli71@gmail.com>
2026-03-24 13:54:16 +0900

    Update stampcard.py

--- cogs/stampcard.py
+++ cogs/stampcard.py
@@ -110,7 +110,7 @@ class StampCard(commands.Cog):
             empty = empty.resize((220, 220))
             stamp = stamp.resize((220, 220))
 
-            start_x = 90
+            start_x = 80
             start_y = 200
 
             gap_x = 240
