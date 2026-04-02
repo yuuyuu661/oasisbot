2813ec542a67452911bf64a46ea4bff97a490612
yuuyuu661 <liuliliuli71@gmail.com>
2026-04-02 10:10:33 +0900

    Update db.py

--- db.py
+++ db.py
@@ -12,7 +12,9 @@ JST = timezone(timedelta(hours=9))
 
 load_dotenv()
 
-RACE_TIMES = ["09:00", "12:00", "15:00", "18:00", "21:00"]
+NORMAL_RACE_TIMES = ["09:00", "12:00", "15:00", "18:00", "21:00"]
+SPECIAL_RACE_TIME = "23:00"
+
 DISTANCES = ["短距離", "マイル", "中距離", "長距離"]
 SURFACES = ["芝", "ダート"]
 CONDITIONS = ["良", "稍重", "重", "不良"]
